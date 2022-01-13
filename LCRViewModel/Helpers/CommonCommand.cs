24839746ee60032ec11b33b7d65514225a2c24d3
Pantec <calejandrofr@gmail.com>
2022-01-13 15:42:40 -0600

    Add ViewModel Projects And Command Helper

---
+++ LCRViewModel/Helpers/CommonCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace LCRViewModel.Helpers
+{
+    public class CommonCommand : ICommand
+    {
+        private Action<object> ExecuteAction;
+        private Func<object, bool> IsExecutable;
+
+        public event EventHandler CanExecuteChanged;
+
+        public CommonCommand(Action<object> executeAction, Func<object, bool> isExecutable) {
+            ExecuteAction = executeAction;
+            IsExecutable = isExecutable;
+        }
+
+        public void CheckIsExecutable() {
+            if (CanExecuteChanged != null)
+            {
+                CanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return IsExecutable(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            ExecuteAction(parameter);
+        }
+    }
+}
