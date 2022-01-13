e5fff57653b0db43ae69f623adecb54b0b284197
Pantec <calejandrofr@gmail.com>
2022-01-13 15:32:31 -0600

    Add Model Project with Entities

---
+++ LCRModel/Entities/Dice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LCRModel.Entities
+{
+    public class Dice
+    {
+        private Random _diceRoller;
+
+        public Dice() { _diceRoller = new Random(); }
+
+        public DieFace Roll()
+        {
+            var result = _diceRoller.Next(0, 5);
+            return Enum.IsDefined(typeof(DieFace), result) ? (DieFace)result : DieFace.DOT;
+        }
+    }
+
+    public enum DieFace
+    {
+        L = 0,
+        C = 1,
+        R = 2,
+        DOT = 3
+    }
+}
---
+++ LCRModel/Entities/Player.cs
@@ -0,0 +1,18 @@
+﻿namespace LCRModel.Entities
+{
+    public class Player
+    {
+        public int Chips { get; set; }
+        public int PlayerTurn { get; }
+
+        public Player(int playerTurn, int initialChipCount)
+        {
+            PlayerTurn = playerTurn;
+            Chips = initialChipCount;
+        }
+
+        public DieFace RollDice(Dice _dice) {
+            return _dice.Roll();
+        }
+    }
+}
